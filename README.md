cab3be3d8512a16befe731c33b35686d4697f334
FereWulf <finn@toxicflames.co.uk>
2026-05-26 13:05:23 +0100

    Added icon + more Shetland relevant feeds

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # Shetland-RSS-Feed
 A feed to get all Shetland news posted on google in the past week, sorted by date
+
+![Main menu](/readme-images/main.png)
+
+![Example of selecting Shetland feed](/readme-images/page.png)
---
+++ Shetland-RSS-Feed/src/feed.js
@@ -0,0 +1,116 @@
+const fetch = window.__TAURI__.http.fetch;
+const opener = window.__TAURI__.opener.openUrl;
+
+let page;
+let dataContainer;
+let refreshBtn;
+
+async function getFeed() {
+	refreshBtn.disabled = true;
+
+	const response = await fetch(page, {
+		method: "GET",
+	})
+	.then(response => {
+		return response.text();
+	})
+	.then(html => {
+		const parser = new DOMParser();
+		const doc = parser.parseFromString(html, "text/html");
+		dataContainer.innerHTML = "";
+
+		var items = doc.getElementsByTagName("item");
+		var sortedItems = [];
+		for (const item of doc.getElementsByTagName("item")) {
+			var count = 0;
+
+			for (const sorted of sortedItems) {
+				const currentDate = Date.parse(sorted.querySelector("pubdate").innerHTML);
+				const newDate = Date.parse(item.querySelector("pubdate").innerHTML);
+
+				if (currentDate < newDate)
+					break;
+
+				count++;
+			}
+
+			if (count == sortedItems.Length)
+				sortedItems.push(item);
+			else
+				sortedItems.splice(count, 0, item);
+		}
+
+		for (const item of sortedItems) {
+			var a = document.createElement("a");
+			a.href = item.childNodes[2].data;
+
+			var title = document.createElement("h3");
+			title.innerHTML = item.querySelector("title").innerHTML.split(" - ")[0];
+			a.appendChild(title);
+
+			var div = document.createElement("div");
+			a.appendChild(div);
+
+			var date = document.createElement("p");
+			date.innerHTML = item.querySelector("pubdate").innerHTML;
+			div.appendChild(date);
+
+			var dash = document.createElement("p");
+			dash.innerHTML = "-";
+			div.appendChild(dash);
+
+			var date = document.createElement("p");
+			date.innerHTML = item.childNodes[7].data;
+			div.appendChild(date);
+
+			dataContainer.appendChild(a);
+
+			//var img = document.createElement("img");
+			//dataContainer.appendChild(img);
+			//setImage(img, item.getElementsByTagName("source")[0].getAttribute("url"));
+		}
+
+		refreshBtn.disabled = false;
+	})
+	.catch(error => {
+		dataContainer.innerHTML = "Failed to fetch feed";
+	})
+}
+
+async function setImage(Image, url) {
+	const response = await fetch(url, {
+		method: "GET",
+	})
+	.then(response => {
+		return response.text();
+	})
+	.then(html => {
+		const parser = new DOMParser();
+		const doc = parser.parseFromString(html, "text/html");
+
+		// TODO: GET ARTICLE IMAGE IF AVAILABLE
+	})
+}
+
+window.addEventListener("DOMContentLoaded", () => {
+	page = window.location.search.split("/?(.*)/s").pop();
+	page = page.substring(1);
+
+	dataContainer = document.querySelector("#data");
+	refreshBtn = document.querySelector("#refresh-btn");
+
+	refreshBtn.addEventListener("click", (e) => {
+		getFeed();
+	});
+
+	document.addEventListener("click", e => {
+		var link = e.target.closest("a");
+		
+		if (link != null && link.id != "back") {
+			e.preventDefault();
+			opener(link.href);
+		}
+	});
+
+	getFeed();
+});
